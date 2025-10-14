8488ef17bf7d2bd7698be9e56960fa868ab2363a
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-14 17:48:27 +0200

    Rebase from 'release/jazzy/coal'

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,3 +1,17 @@
+#.rst:
+# Version
+# -----
+# The version of the project is defined directly from git.
+# If you wish to create a release :
+#.. code-block:: bash
+#
+#  make release VERSION=X.Y.Z
+#
+# The system then tries to create a compressed tar file.
+# It is then build, and install.
+# If this is working a signature is created.
+#
+
 # Copyright (C) 2008-2019 LAAS-CNRS, JRL AIST-CNRS, INRIA.
 #
 # This program is free software: you can redistribute it and/or modify it under
@@ -235,9 +249,9 @@ macro(VERSION_COMPUTE)
   if(PROJECT_VERSION)
     # Compute the major, minor and patch version of the project
     if(
-      NOT DEFINED PROJECT_VERSION_MAJOR
-      AND NOT DEFINED PROJECT_VERSION_MINOR
-      AND NOT DEFINED PROJECT_VERSION_PATCH
+      NOT PROJECT_VERSION_MAJOR
+      AND NOT PROJECT_VERSION_MINOR
+      AND NOT PROJECT_VERSION_PATCH
     )
       SPLIT_VERSION_NUMBER(
         ${PROJECT_VERSION}
