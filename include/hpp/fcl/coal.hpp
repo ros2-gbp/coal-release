ac9ad88dc179b120f1557a2eb54e95a82d0416b5
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-14 17:40:09 +0200

    Imported upstream version '3.0.2' of 'upstream'

--- include/hpp/fcl/coal.hpp
+++ include/hpp/fcl/coal.hpp
@@ -7,7 +7,10 @@
 #define COAL_BACKWARD_COMPATIBILITY_WITH_HPP_FCL
 
 // Don't setup warning because there can be ignored by -isystem
-#pragma message("Please update your includes from 'hpp/fcl' to 'coal'")
+#ifndef COAL_DISABLE_HPP_FCL_WARNINGS
+#pragma message( \
+    "Please update your includes from 'hpp/fcl' to 'coal' or define COAL_DISABLE_HPP_FCL_WARNINGS")
+#endif
 
 #define HPP_FCL_VERSION_AT_LEAST(major, minor, patch) \
   COAL_VERSION_AT_LEAST(major, minor, patch)
